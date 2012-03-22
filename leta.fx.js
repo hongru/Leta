b04dd2f50ab188fbb267bb940ebd19d206df312d
hongru <hongru.chenhr@gmail.com>
2012-03-22 10:48:31 +0800

    update leta.fx

--- leta.fx.js
+++ leta.fx.js
@@ -83,17 +83,18 @@
     } ()
 
     function parseTransform(style, base) {
-        var values = {},
-        m
-        if (m = style.match(rotate)) values.rotate = by(m[1], base ? base.rotate: null) if (m = style.match(scale)) values.scale = by(m[1], base ? base.scale: null) if (m = style.match(skew)) {
+        var values = {}, m;
+        if (m = style.match(rotate)) values.rotate = by(m[1], base ? base.rotate: null); 
+		if (m = style.match(scale)) values.scale = by(m[1], base ? base.scale: null); 
+		if (m = style.match(skew)) {
             values.skewx = by(m[1], base ? base.skewx: null);
-            values.skewy = by(m[3], base ? base.skewy: null)
+            values.skewy = by(m[3], base ? base.skewy: null);
         }
         if (m = style.match(translate)) {
             values.translatex = by(m[1], base ? base.translatex: null);
-            values.translatey = by(m[3], base ? base.translatey: null)
+            values.translatey = by(m[3], base ? base.translatey: null);
         }
-        return values
+        return values;
     }
 
     function formatTransform(v) {
